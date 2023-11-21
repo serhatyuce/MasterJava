d9c1b0ebfa9fb344dbc9cce9b6baa84d61c2f420
serhatyuce <yuce.serhat@outlook.com>
2023-11-21 22:34:48 +0300

    switch statement

---
+++ switch_/switchStatement.java
@@ -0,0 +1,30 @@
+package switch_;
+
+public class switchStatement {
+
+    public void statementOfSwitch(int switchValue) {
+
+        switch (switchValue) {
+            case 1 -> System.out.println("value is 1");
+            case 2 -> System.out.println("value is 2");
+            case 3, 4, 5 -> System.out.println("value is 3,4 or 5");
+            default -> System.out.println("bad request.");
+        }
+    }
+
+    public void selectQuarter(String month){
+        System.out.println(month + " is in the " + statementOfSwitch(month) + " quarter");
+    }
+
+    public String statementOfSwitch(String month){
+
+        return switch (month){
+            case "january", "february", "march" -> {yield "1st";}
+            case "april", "may", "june" -> "2st";
+            case "july", "august", "september" -> "3th";
+            case "october", "november", "december" -> "4th";
+            default -> "bad";
+        };
+    }
+
+}
---
+++ switch_/switchStatementTest.java
@@ -0,0 +1,14 @@
+package switch_;
+
+public class switchStatementTest {
+
+    public static void main(String[] args) {
+
+        switchStatement switchIt = new switchStatement();
+
+        switchIt.statementOfSwitch(3);
+        switchIt.selectQuarter("march");
+
+    }
+
+}
